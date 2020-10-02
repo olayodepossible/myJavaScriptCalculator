81939590f6aadb5fe60ebb1d06c14a10f1d7bb66
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-10-02 20:17:16 +0100

    javascript file refactored

--- cal.js
+++ cal.js
@@ -1,37 +1,54 @@
-function calculate(){
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-    let operator = document.getElementById('operators').value;
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = calculator.querySelector(".calculator__display");
 
-    switch(operator){
-        case '+':
-            document.getElementById('result').value = num1 + num2;
-            break;
-        case '-':
-            document.getElementById('result').value = num1 - num2;
-            break;
-        case '*':
-            document.getElementById('result').value = num1 * num2;
-            break;
-        case '/':
-            document.getElementById('result').value = num1 / num2;
-            break;
-        case '%':
-            document.getElementById('result').value = num1 % num2;
-            break;
-        case 'CE':
-            document.getElementById('num1').value = 0;
-            document.getElementById('num2').value = 0;
-            document.getElementById('result').value = 0;
-            break;
+keys.addEventListener("click", (event) => {
+  if (!event.target.closest("button")) return;
 
+  const key = event.target;
+  const keyValue = key.textContent;
+  const displayValue = display.textContent;
+  const { type } = key.dataset;
+  const { previousKeyType } = calculator.dataset;
+
+  if (type === "number") {
+    if (displayValue === "0") {
+      display.textContent = keyValue;
+    } else if (previousKeyType === "operator") {
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
     }
+  }
+
+  if (type === "operator") {
+    const operatorKeys = keys.querySelectorAll('[data-type = "operator"]');
+    operatorKeys.forEach((element) => {
+      element.dataset.state = "";
+    });
+    key.dataset.state = "selected";
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  if (type === "equal") {
+    const firstNumber = parseInt(calculator.dataset.firstNumber);
+    const operator = calculator.dataset.operator;
+    const secondNumber = parseInt(displayValue);
+
+    display.textContent = doCalculation(firstNumber, operator, secondNumber);
+  }
+
+  calculator.dataset.previousKeyType = type;
+});
+
+function doCalculation(firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+
+  if (operator === "plus") return firstNumber + secondNumber;
+  if (operator === "minus") return firstNumber - secondNumber;
+  if (operator === "times") return firstNumber * secondNumber;
+  if (operator === "divide") return firstNumber / secondNumber;
 }
-
-document.getElementById('button').addEventListener('click', calculate);
-
-
-
-
-
-
